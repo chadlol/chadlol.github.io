a898f8ddfce5c0e2ff81f8dede4a6fb1ac5d236c
Chad Ohl <chadohl@gmail.com>
2019-03-13 11:27:59 -0700

    Addeded gulp tasks; Added fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('html', function(){
+    return gulp.src('src/*.html')
+      .pipe(gulp.dest('dist'))
+});
+
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/*')
+      .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('sass', function(){
+    return gulp.src('src/scss/styles.scss')
+      .pipe(sass()) // Using gulp-sass
+      .pipe(gulp.dest('dist/css'))
+});
+
+
