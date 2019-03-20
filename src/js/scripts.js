545c7949510625a85161586dc014fed9516a722e
Chad Ohl <chadohl@gmail.com>
2019-03-20 13:16:19 -0700

    Adjusted Header styling; Collapsed nav bar now collapases on screen resize; Updated gitignore

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,6 +5,27 @@ let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-    
     navBar.classList.toggle('active');
 });
+
+const delay = 100;  // Your delay here
+
+const originalResize = evt => {
+    navBar.classList.remove('active');
+};
+
+
+(() => {
+  resizeTaskId = null;
+
+  window.addEventListener('resize', evt => {
+    if (resizeTaskId !== null) {
+      clearTimeout(resizeTaskId);
+    }
+
+    resizeTaskId = setTimeout(() => {
+      resizeTaskId = null;
+      originalResize(evt);
+    }, delay);
+  });
+})();
