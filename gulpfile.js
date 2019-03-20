e1fb4b3c57f81072244b88f63ebaa4c1bf9b78e0
Chad Ohl <chadohl@gmail.com>
2019-03-19 17:22:19 -0700

    Added typography styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ var sass = require('gulp-sass');
 
 gulp.task('html', function(){
     return gulp.src('src/*.html')
-      .pipe(gulp.dest('dist'))
+      .pipe(gulp.dest('./'))
 });
 
 gulp.task('fonts', function(){
     return gulp.src('src/fonts/*')
-      .pipe(gulp.dest('dist/fonts'))
+      .pipe(gulp.dest('./fonts'))
 });
 
 gulp.task('sass', function(){
     return gulp.src('src/scss/styles.scss')
       .pipe(sass()) // Using gulp-sass
-      .pipe(gulp.dest('dist/css'))
+      .pipe(gulp.dest('./css'))
 });
