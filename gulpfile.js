98ded8f0a9a083a4d992e276edb0f6c3a03456b5
Chad Ohl <chadohl@gmail.com>
2019-03-19 20:02:14 -0700

    Added browserSync; Updated Header; Updated Nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('html', function(){
     return gulp.src('src/*.html')
       .pipe(gulp.dest('./'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
 });
 
 gulp.task('fonts', function(){
@@ -15,4 +19,30 @@ gulp.task('sass', function(){
     return gulp.src('src/scss/styles.scss')
       .pipe(sass()) // Using gulp-sass
       .pipe(gulp.dest('./css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+});
+
+gulp.task('js', function(){
+  return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('src/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('src/**/*.html', ['html']); 
+  gulp.watch('src/**/*.js', ['js']); 
 });
