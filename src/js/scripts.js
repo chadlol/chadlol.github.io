4006eb26b1d030dbc49f8aba9a069ce0e399d9ac
Chad Ohl <chadohl@gmail.com>
2019-03-19 23:36:00 -0700

    Updated header and nav styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,10 @@
 console.log('OK');
 
+let navBar = document.getElementById('navbar');
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
     
-    mainNav.classList.toggle('active');
+    navBar.classList.toggle('active');
 });
