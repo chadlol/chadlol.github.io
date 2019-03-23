5f60a46e083f51a1611774b648be544d4c714f8c
Chad Ohl <chadohl@gmail.com>
2019-03-22 20:39:08 -0700

    Updated images task move subdirectories

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('fonts', function(){
 });
 
 gulp.task('images', function(){
-  return gulp.src('src/images/*')
+  return gulp.src('src/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./images'))
     
