dc8145cfb1c81f60e56ef9c0bdee393929078780
Chad Ohl <chadohl@gmail.com>
2021-07-26 14:58:33 -0700

    Added email link

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+console.log('OK');
+
 let navBar = document.getElementById('navbar');
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
